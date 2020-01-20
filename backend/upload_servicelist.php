62c22c64c309f403144269deffb6b8238e9f8132
Tomi Sarparanta <tomi.sarparanta@sofiadigital.com>
2020-01-20 15:08:46 +0200

    Fixed service list upload and name verification

--- backend/upload_servicelist.php
+++ backend/upload_servicelist.php
@@ -2,7 +2,7 @@
 $target_dir = "servicelists/";
 if(isset($_POST['servicelist']) && isset($_POST['filename'])) {
     $data = $_POST['servicelist'];
-    $filename = $target_dir .$_POST['filename'];
+    $filename = $_POST['filename'];
 
     if (strpos($filename, '/') !== false || strpos($filename, '..') !== false) {
        http_response_code (400);
@@ -13,10 +13,11 @@ if(isset($_POST['servicelist']) && isset($_POST['filename'])) {
     if(substr($filename, -4) != ".xml") {
         $filename = $filename.".xml";
     }
-    if (!file_exists($filename)) {
-        $fh = fopen($filename, 'w') or die("Can't create file");
+    $target = $target_dir.$filename;
+    if (!file_exists($target)) {
+        $fh = fopen($target , 'w') or die("Can't create file");
     }
-    $ret = file_put_contents($filename, $data,LOCK_EX);
+    $ret = file_put_contents($target, $data,LOCK_EX);
     if($ret === false) {
         http_response_code (400);
         echo('There was an error writing this file');
