53e2b2c38aad0e321e70cc3c0e69321fe568e19c
Tomi Sarparanta <tomi.sarparanta@sofiadigital.com>
2020-02-12 10:23:41 +0200

    Indent service list when saving

--- backend/upload_servicelist.php
+++ backend/upload_servicelist.php
@@ -17,7 +17,18 @@ if(isset($_POST['servicelist']) && isset($_POST['filename'])) {
     if (!file_exists($target)) {
         $fh = fopen($target , 'w') or die("Can't create file");
     }
-    $ret = @file_put_contents($target, $data,LOCK_EX);
+
+    $dom = new DOMDocument();
+
+    // Initial block (must before load xml string)
+    $dom->preserveWhiteSpace = false;
+    $dom->formatOutput = true;
+    // End initial block
+
+    $dom->loadXML($data);
+    $out = $dom->saveXML();
+
+    $ret = @file_put_contents($target, $out,LOCK_EX);
     if($ret === false) {
         http_response_code (400);
         echo('There was an error writing this file');
