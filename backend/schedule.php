2409d5c137929e8f5d1926a7d61521ed5259fd82
Tomi Sarparanta <tomi.sarparanta@sofiadigital.com>
2020-02-19 14:07:35 +0200

    Correct ZuluTime timeformat for content guide

--- backend/schedule.php
+++ backend/schedule.php
@@ -2,6 +2,7 @@
 //TODO 
 header('Content-Type: text/xml');
 header("Access-Control-Allow-Origin: *");
+$timeformat = "Y-m-d\TH:i:s\Z";
 date_default_timezone_set ("Zulu");
 if(isset($_GET['now_next']) && isset($_GET['sid']) && $_GET['now_next'] == "true" ) {
     $present_start = rand(1,45);
@@ -12,9 +13,9 @@ if(isset($_GET['now_next']) && isset($_GET['sid']) && $_GET['now_next'] == "true
     $following_starttime = $present_starttime + ($present_duration *60);
     $following_endtime =  $following_starttime + ($following_duration * 60);
     $schedule= file_get_contents("nownext.xml");
-    $schedule =str_replace("2013-09-25T11:15:00Z",date("c", $present_starttime),$schedule);
-    $schedule =str_replace("2013-09-25T12:00:00Z",date("c", $following_starttime),$schedule);
-    $schedule =str_replace("2013-09-25T12:30:00.000Z",date("c", $following_endtime),$schedule);
+    $schedule =str_replace("2013-09-25T11:15:00Z",date($timeformat, $present_starttime),$schedule);
+    $schedule =str_replace("2013-09-25T12:00:00Z",date($timeformat, $following_starttime),$schedule);
+    $schedule =str_replace("2013-09-25T12:30:00.000Z",date($timeformat, $following_endtime),$schedule);
     $schedule =str_replace("SERVICE_ID_TEMPLATE",$_GET['sid'],$schedule);
     echo $schedule;
 }
@@ -31,7 +32,7 @@ else if(isset($_GET['start']) && isset($_GET['end']) && isset($_GET['sids']) ){
     while($start < $schedule_end) {
         $schedule = file_get_contents("schedule_event_template.xml");
         $schedule =str_replace("PROGRAM_ID_TEMPLATE",$id,$schedule);
-        $schedule =str_replace("START_TIME_TEMPLATE",date("c", $start),$schedule);
+        $schedule =str_replace("START_TIME_TEMPLATE",date($timeformat, $start),$schedule);
         $schedule =str_replace("DURATION_TEMPLATE","PT".$program_length."M",$schedule);
         $schedules = $schedules.$schedule;
         $program = file_get_contents("program_information_template.xml");
@@ -44,8 +45,8 @@ else if(isset($_GET['start']) && isset($_GET['end']) && isset($_GET['sids']) ){
     }
     $schedule_document = file_get_contents("schedule_template.xml");
     $schedule_document =str_replace( "SERVICE_ID_TEMPLATE",$_GET['sids'][0],$schedule_document);
-    $schedule_document =str_replace( "START_TEMPLATE",date("c", $schedule_start),$schedule_document);
-    $schedule_document =str_replace( "END_TEMPLATE",date("c", $schedule_end),$schedule_document);
+    $schedule_document =str_replace( "START_TEMPLATE",date($timeformat, $schedule_start),$schedule_document);
+    $schedule_document =str_replace( "END_TEMPLATE",date($timeformat, $schedule_end),$schedule_document);
     $schedule_document =str_replace( "<!--PROGRAMS-->",$programs,$schedule_document);
     $schedule_document =str_replace( "<!--SCHEDULES-->",$schedules,$schedule_document);
     echo $schedule_document;
