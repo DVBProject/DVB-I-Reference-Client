b72aa7e54af002b169c2263ea0aa5c276e4d67c9
Tomi Sarparanta <tomi.sarparanta@sofiadigital.com>
2021-06-14 14:16:46 +0300

    Default install location points to the stage installation

--- backend/configuration.php
+++ backend/configuration.php
@@ -3,8 +3,6 @@
 // install_location is the installation directory
 // see also dvbi-common.js
 //
-# $install_location = "http://stage.sofiadigital.fi/dvb/dvb-i-reference-application";
- 
-$install_location = "http://paulhiggs.ddns.net:8118";
+$install_location = "https://stage.sofiadigital.fi/dvb/dvb-i-reference-application";
 
 ?>
