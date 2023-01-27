797d0cea75bf716b5cfe577fe78e48d57fb8ac05
Paul Higgs <paul_higgs@hotmail.com>
2023-01-27 23:49:52 +0000

    dont show non-LCN services

--- frontend/configuration.js
+++ frontend/configuration.js
@@ -1,2 +1,5 @@
 // DVB-I Reference installation location -- also backend/configuration.php
 var INSTALL_LOCATION="https://stage.sofiadigital.fi/dvb/dvb-i-reference-application";
+
+// set to true to include <Service> channels that are not included in the selected LCN table.
+var INCLUDE_NON_LCN_CHANNELS=false;
