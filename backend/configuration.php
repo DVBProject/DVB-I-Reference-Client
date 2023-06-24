655ee049bd75c88eb51d1ed38a3f30acb5d14abd
Paul Higgs <paul_higgs@hotmail.com>
2023-06-24 06:51:37 +0100

    remove services that are no longer accessible. fixes #79

--- backend/configuration.php
+++ backend/configuration.php
@@ -3,5 +3,5 @@
 // install_location is the installation directory
 // see also frontend/configuration.js
 //
-$install_location = "https://stage.sofiadigital.fi/dvb/dvb-i-reference-application";
+$install_location = "http://paulhiggs.ddns.net:8877/pauls";
 ?>
--- frontend/configuration.js
+++ frontend/configuration.js
@@ -1,5 +1,5 @@
 // DVB-I Reference installation location -- also backend/configuration.php
-var INSTALL_LOCATION = "https://stage.sofiadigital.fi/dvb/dvb-i-reference-application";
+var INSTALL_LOCATION = "http://paulhiggs.ddns.net:8877/pauls";
 
 // set to true to include <Service> channels that are not included in the selected LCN table.
-var INCLUDE_NON_LCN_CHANNELS = true;
+var INCLUDE_NON_LCN_CHANNELS = false;
