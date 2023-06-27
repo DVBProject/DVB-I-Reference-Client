a1c51265559db906e68e8d32c163657c957a15df
Paul Higgs <paul_higgs@hotmail.com>
2023-06-27 09:15:26 +0100

    revert to deployed backend

--- frontend/configuration.js
+++ frontend/configuration.js
@@ -1,5 +1,5 @@
 // DVB-I Reference installation location -- also backend/configuration.php
-var INSTALL_LOCATION = "http://paulhiggs.ddns.net:8877/pauls";
+var INSTALL_LOCATION = "https://stage.sofiadigital.fi/dvb/dvb-i-reference-application";
 
 // set to true to include <Service> channels that are not included in the selected LCN table.
 var INCLUDE_NON_LCN_CHANNELS = false;
