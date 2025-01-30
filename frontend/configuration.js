ae13692081ff6f1768ce9923166acd9ba85e4a77
Rafael Bermudez Guijo <rbermudez.h@ccma.cat>
2025-01-30 11:20:54 +0100

    build: Move default location to /client to mantain compatibility

--- frontend/configuration.js
+++ frontend/configuration.js
@@ -1,5 +1,5 @@
 // DVB-I Reference installation location -- also backend/configuration.php
-var INSTALL_LOCATION = "";
+var INSTALL_LOCATION = "/client";
 
 // set to true to include <Service> channels that are not included in the selected LCN table.
 var INCLUDE_NON_LCN_CHANNELS = false;
