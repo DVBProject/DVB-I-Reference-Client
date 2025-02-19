8c55b0349da0525efb225477a322d922e8c60001
Paul Higgs <paul_higgs@hotmail.com>
2025-02-19 08:41:29 +0000

    Out-of-service image is in the object

--- frontend/android/js/channel.js
+++ frontend/android/js/channel.js
@@ -236,7 +236,7 @@ Channel.prototype.channelSelected = function () {
       $("#notification").removeClass();
       $("#notification").addClass("noservice");
       if (self.out_of_service_image) {
-        $("#notification").html('<img src="' + self.out_of_service_image + '" class="img-fluid position-relative"/>');
+        $("#notification").html('<img src="' + self.out_of_service_image.mediaUri + '" class="img-fluid position-relative"/>');
       } else {
         $("#notification").text("Service not available");
       }
