b2d24f960ca620cf9ebe3abf2c0495e3eb1e519c
Paul Higgs <paul_higgs@hotmail.com>
2025-02-20 09:14:53 +0000

    accept all types (for servers not recignising DVBI content type

--- frontend/network.js
+++ frontend/network.js
@@ -4,7 +4,8 @@ function NetworkRequest(url, settings) {
   settings.beforeSend = function (req) {
     // Chrome:: Refused to set unsafe header "User-Agent"
     // req.setRequestHeader("User-Agent", "DVB-I/A177r6");
-    req.setRequestHeader("Accept", "application/vnd.dvb.dvbi.r6");
+
+    req.setRequestHeader("Accept", "text/plain, */*, application/vnd.dvb.dvbi.r6");
   };
 
   settings.url = url;
