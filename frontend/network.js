207125a45eee843a08367388123f4f0069d9ad62
Paul Higgs <paul_higgs@hotmail.com>
2026-06-18 07:04:00 +0100

    set User-Agent and accepts info to A177r8

--- frontend/network.js
+++ frontend/network.js
@@ -1,10 +1,10 @@
 function NetworkRequest(url, settings) {
-  // settings.accepts= { mycustomtype: "application/vnd.dvb.dvbi.r6" },
+  // settings.accepts= { mycustomtype: "application/vnd.dvb.dvbi.r8" },
 
   settings.beforeSend = function (req) {
     // Chrome:: Refused to set unsafe header "User-Agent"
-    // req.setRequestHeader("User-Agent", "DVB-I/A177r6");
-    req.setRequestHeader("Accept", "text/plain, */*, application/vnd.dvb.dvbi.r6");
+    // req.setRequestHeader("User-Agent", "DVB-I/A177r8");
+    req.setRequestHeader("Accept", "text/plain, */*, application/vnd.dvb.dvbi.r8");
   };
 
   settings.url = url;
